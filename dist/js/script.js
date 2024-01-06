6a4cbe0b2be2a05f88293dff01a7581ca4956589
Alafrah4567 <142050022+Alafrah4567@users.noreply.github.com>
2024-01-06 09:10:13 -0800

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+
+const mobilebtn =document.querySelector('#mobilebtn');
+const mobilemenu =document.querySelector('#mobilemenu');
+
+
+mobilebtn.addEventListener('click', ()=>{
+   mobilemenu.classList.toggle('hidden')
+});
